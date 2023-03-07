c1e4e3f9fedc678f6b1374114c20dee3d5dd1e72
yurbly <yunov1@gmail.com>
2023-03-07 17:13:28 +0400

    deploy to github pages

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
   },
   plugins: [new HtmlWebpackPlugin({
     template: './public/index.html',
